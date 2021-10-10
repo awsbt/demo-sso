496dc775e7123af941e6f4fb1ce2e2e537e7d3fd
Bill Tsai <bill_tsai_va@hotmail.com>
2021-10-10 14:59:01 -0400

    fr saml works! need to change loginURL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@ export const environment = {
   sso_api_username: '28omb38m5vuf3bv2dpa1efl4bv',
   sso_api_pwd: '1cdr9hv9d8o3r5e6u48t7pjqu82lue8nidq49t46s3tmg4i7b4mf',
 
-  loginURL: 'https://fr-pools.auth-fips.us-gov-west-1.amazoncognito.com/login?' +
+  loginURL: 'https://fr-pools.auth-fips.us-gov-west-1.amazoncognito.com/oauth2/authorize?' +
               'client_id=28omb38m5vuf3bv2dpa1efl4bv&response_type=code&scope=openid+profile&' +
-              'redirect_uri=http://localhost:4200/callback',
+              'redirect_uri=http://localhost:4200/callback&idp_identifier=fr-idp',
 
   redirectURL: 'http://localhost:4200/callback',
 
